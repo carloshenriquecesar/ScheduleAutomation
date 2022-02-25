9b5401b3704b368260d870f324f0765063904460
Carlos <carloshenriquecesar@live.com>
2022-02-25 16:21:24 -0300

    Melhoria na runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,6 +3,7 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "C:/Users/carlo/Desktop/Desafio/ScheduleAutomation/src/test/resources/features",
@@ -11,4 +12,11 @@ import io.cucumber.testng.CucumberOptions;
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
+    //@DataProvider(parallel = true)
+    @DataProvider(parallel = false)
+    @Override
+    public Object[][] scenarios(){
+            return super.scenarios();
+    }
+
 }
